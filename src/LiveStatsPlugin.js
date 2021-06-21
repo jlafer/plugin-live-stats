c9b021b76b7698d62e3d75a754a05c1c73e39084
John Lafer <jlafer@yahoo.com>
2021-06-21 16:34:28 -0700

    moved all config work to pluginConfig.js

--- src/LiveStatsPlugin.js
+++ src/LiveStatsPlugin.js
@@ -1,4 +1,3 @@
-import * as R from 'ramda';
 import React from 'react';
 import * as Flex from '@twilio/flex-ui';
 import { FlexPlugin } from 'flex-plugin';
@@ -8,92 +7,11 @@ import StatsPage from "./components/StatsPage/StatsPageContainer";
 import SidebarStatsButton from './components/SidebarStatsButton/SidebarStatsButton';
 import reducers, {namespace, initQueries} from './states';
 import {startLiveQueries} from './statsMgmt';
-import {verifyAndFillConfiguration} from './pluginConfig';
+import {
+  addConfigurationToSchema, addWorkerActivityFilterDefn, verifyAndFillConfiguration
+} from './pluginConfig';
 
 const PLUGIN_NAME = 'LiveStatsPlugin';
-
-const baseSchema = {
-  workers: {
-    index: 'tr-worker',
-    key: 'sid',
-    columns: [
-      { id: 'agentName', numeric: false, disablePadding: true, label: 'Name' },
-      { id: 'activityStr', numeric: false, disablePadding: false, label: 'Activity' },
-      { id: 'formattedAge', numeric: true, disablePadding: false, label: 'Activity Time', sortFld: 'activityAge' },
-      { id: 'tasksStr', numeric: false, disablePadding: false, label: 'Tasks' },
-      { id: 'skillsStr', numeric: false, disablePadding: false, label: 'Skills' }
-    ],
-    defaultSortCol: 'agentName',
-    filterDefns: []
-  },
-  tasks: {
-    index: 'tr-task',
-    key: 'sid',
-    columns: [
-      { id: 'from', numeric: false, disablePadding: true, label: 'From' },
-      { id: 'status', numeric: false, disablePadding: true, label: 'Status' },
-      { id: 'queue_name', numeric: false, disablePadding: true, label: 'Queue' },
-      { id: 'formattedAge', numeric: true, disablePadding: false, label: 'Task Time', sortFld: 'statusAge' },
-      { id: 'channel_type', numeric: false, disablePadding: false, label: 'Channel' },
-      { id: 'worker_name', numeric: false, disablePadding: false, label: 'Agent' },
-    ],
-    defaultSortCol: 'queue_name',
-    filterDefns: [
-      {
-        name: 'status', label: 'Status', field: 'status',
-        options: [
-          {label: 'All', name: 'All'},
-          {label: 'Queued', name: 'pending'},
-          {label: 'Alerting', name: 'reserved'},
-          {label: 'Assigned', name: 'assigned'},
-          {label: 'Wrapping', name: 'wrapping'}
-        ]
-      }
-    ],
-  }
-};
-
-const nameToNameAndLabelObj = (name, nameToLabelFn) => {
-  const label = nameToLabelFn ? nameToLabelFn(name) : name;
-  return ({name: name, label: label})
-};
-
-const addWorkerActivityFilterDefn = (store, schema) => {
-  const {workers} = schema;
-  const {filterDefns} = workers;
-  const activities = Object.fromEntries(store.getState().flex.worker.activities);
-  const activityOptions = R.pipe(R.values, R.map(R.prop('name')), R.map(nameToNameAndLabelObj), R.append({name: 'All', label: 'All'}))(activities);
-  const activityFilter = {
-    name: 'activity', label: 'Activity', field: 'activity_name',
-    options: activityOptions
-  };
-  const filterDefnsWithActivity = R.append(activityFilter, filterDefns);
-  const workersSchemaWithActivity = {...workers, filterDefns: filterDefnsWithActivity};
-  return {...schema, workers: workersSchemaWithActivity}
-};
-
-const addConfigurationToSchema = (base, config) => {
-  const {tasks, workers} = base;
-  const tasksConfigured = addConfigurationToTbl(tasks, config.tasks);
-  const workersConfigured = addConfigurationToTbl(workers, config.workers);
-  return {tasks: tasksConfigured, workers: workersConfigured};
-};
-
-const addConfigurationToTbl = (base, toAdd) => {
-  return {...base,
-    filterDefns: R.concat(base.filterDefns, toAdd.filterDefns),
-    columns: mergeColumns(base.columns, toAdd.columns)
-  }
-};
-
-const mergeColumns = (baseCols, cfgCols) => {
-  return R.map( mergeColumn(baseCols), cfgCols )
-};
-
-const mergeColumn = R.curry((baseCols, cfgCol) => {
-  const baseCol = R.find(col => col.id === cfgCol.id, baseCols);
-  return R.mergeDeepRight(baseCol, cfgCol);
-});
 
 export default class LiveStatsPlugin extends FlexPlugin {
   constructor() {
@@ -107,7 +25,7 @@ export default class LiveStatsPlugin extends FlexPlugin {
     const rawConfig = getPluginConfiguration(manager, 'plugin_live_stats');
     const config = verifyAndFillConfiguration(rawConfig);
     console.log(`${PLUGIN_NAME}: config:`, config);
-    const schemaWithWorkerActivityFilter = addWorkerActivityFilterDefn(store, baseSchema);
+    const schemaWithWorkerActivityFilter = addWorkerActivityFilterDefn(store);
     const schema = addConfigurationToSchema(schemaWithWorkerActivityFilter, config);
     console.log(`${PLUGIN_NAME}: schema:`, schema);
     manager.store.dispatch( initQueries(schema) );
--- src/pluginConfig.js
+++ src/pluginConfig.js
@@ -1,3 +1,5 @@
+import * as R from 'ramda';
+
 export function verifyAndFillConfiguration(cfg) {
   if (! cfg)
     throw new Error(`LiveStatsPlugin: attributes.plugin_live_stats NOT configured. See README for instructions.`);
@@ -12,3 +14,87 @@ const verifyAndFillTblConfig = (cfg, key) => {
   const {columns, filterDefns} = cfg[key];
   return {...cfg[key], columns: columns};
 }
+
+const baseSchema = {
+  workers: {
+    index: 'tr-worker',
+    key: 'sid',
+    columns: [
+      { id: 'agentName', numeric: false, disablePadding: true, label: 'Name' },
+      { id: 'activityStr', numeric: false, disablePadding: false, label: 'Activity' },
+      { id: 'formattedAge', numeric: true, disablePadding: false, label: 'Activity Time', sortFld: 'activityAge' },
+      { id: 'tasksStr', numeric: false, disablePadding: false, label: 'Tasks' },
+      { id: 'skillsStr', numeric: false, disablePadding: false, label: 'Skills' }
+    ],
+    defaultSortCol: 'agentName',
+    filterDefns: []
+  },
+  tasks: {
+    index: 'tr-task',
+    key: 'sid',
+    columns: [
+      { id: 'from', numeric: false, disablePadding: true, label: 'From' },
+      { id: 'status', numeric: false, disablePadding: true, label: 'Status' },
+      { id: 'queue_name', numeric: false, disablePadding: true, label: 'Queue' },
+      { id: 'formattedAge', numeric: true, disablePadding: false, label: 'Task Time', sortFld: 'statusAge' },
+      { id: 'channel_type', numeric: false, disablePadding: false, label: 'Channel' },
+      { id: 'worker_name', numeric: false, disablePadding: false, label: 'Agent' },
+    ],
+    defaultSortCol: 'queue_name',
+    filterDefns: [
+      {
+        name: 'status', label: 'Status', field: 'status',
+        options: [
+          {label: 'All', name: 'All'},
+          {label: 'Queued', name: 'pending'},
+          {label: 'Alerting', name: 'reserved'},
+          {label: 'Assigned', name: 'assigned'},
+          {label: 'Wrapping', name: 'wrapping'}
+        ]
+      }
+    ],
+  }
+};
+
+const nameToNameAndLabelObj = (name, nameToLabelFn) => {
+  const label = nameToLabelFn ? nameToLabelFn(name) : name;
+  return ({name: name, label: label})
+};
+
+export const addWorkerActivityFilterDefn = (store) => {
+  const {workers} = baseSchema;
+  const {filterDefns} = workers;
+  const activities = Object.fromEntries(store.getState().flex.worker.activities);
+  const activityOptions = R.pipe(R.values, R.map(R.prop('name')), R.map(nameToNameAndLabelObj), R.append({name: 'All', label: 'All'}))(activities);
+  const activityFilter = {
+    name: 'activity', label: 'Activity', field: 'activity_name',
+    options: activityOptions
+  };
+  const filterDefnsWithActivity = R.append(activityFilter, filterDefns);
+  const workersSchemaWithActivity = {...workers, filterDefns: filterDefnsWithActivity};
+  return {...baseSchema, workers: workersSchemaWithActivity};
+};
+
+export const addConfigurationToSchema = (base, config) => {
+  const {tasks, workers} = base;
+  const tasksConfigured = addConfigurationToTbl(tasks, config.tasks);
+  const workersConfigured = addConfigurationToTbl(workers, config.workers);
+  return {tasks: tasksConfigured, workers: workersConfigured};
+};
+
+const addConfigurationToTbl = (base, toAdd) => {
+  return {...base,
+    filterDefns: R.concat(base.filterDefns, toAdd.filterDefns),
+    columns: mergeColumns(base.columns, toAdd.columns)
+  };
+};
+
+const mergeColumns = (baseCols, cfgCols) => {
+  return R.map( mergeColumn(baseCols), cfgCols );
+};
+
+const mergeColumn = R.curry((baseCols, cfgCol) => {
+  const baseCol = R.find(col => col.id === cfgCol.id, baseCols);
+  return R.mergeDeepRight(baseCol, cfgCol);
+});
+
